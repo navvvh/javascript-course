ddf69d23120137a4bfcafc670dcb7a23900c11ec
navvvh <vhanhajjmacaraig21@gmail.com>
2025-08-20 13:08:17 +0800

    - Set up JavaScript environment and browser console - Implement let, const, var variable declarations - Add data type exploration with typeof operator - Establish development workflow and best practices

--- 01-fundamentals-part-1/starter/script.js
+++ 01-fundamentals-part-1/starter/script.js
@@ -1,2 +1,61 @@
 // JavaScript Fundamentals - Part 1
 // We'll write our code here!
+
+let js = "amazing";
+console.log (40 + 30 - 45);
+console.log("GUMANA KA");
+
+console.log ("=== VARIABLES ===");
+
+let firstName = "Hajj"
+console.log (firstName)
+
+let age = 20;
+console.log(age);
+age = 50; 
+console.log(age);
+
+const birthYear = 2005;
+console.log(birthYear);
+
+const PI = 3.1415;
+console.log(PI);
+
+var job = "programmer";
+job = "programmer"
+console.log(job);
+
+age = 22;
+
+let lastName = "Macaraig"
+console.log(lastName)
+
+console.log("=== DATATYPES ===")
+
+age = 24;
+console.log(age)
+
+console.log(typeof age);
+
+let userNAME = "Mark";
+console.log(userNAME);
+
+console.log(typeof userNAME);
+
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+console.log(typeof javaScriptIsFun)
+
+
+let year;
+console.log(year)
+console.log(typeof year);
+
+
+let dynamicVariable = 23;
+console.log(dynamicVariable, typeof dynamicVariable);
+
+dynamicVariable = true;
+console.log(dynamicVariable, typeof dynamicVaribale)
+
+
